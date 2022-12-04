13d7244dd8fb9ac8ea637abfa6e93a728fcb0a56
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2022-12-04 13:05:46 +0300

    Update Lab3.cpp

--- Lab3.cpp
+++ Lab3.cpp
@@ -9,20 +9,23 @@
 
 #include <iostream>
 #include "Graph.h"
+#include <fstream>
 using namespace std;
 
 int step;
+int sw;
 
 void DSF(int** arr, int size, bool* flag, int in, int start, int& count_cycles)
 {
 	step++;
-	cout << in + 1 << " ";
+	cout << in + 1 << "/" << step << " ";
 	flag[in] = true;
-	if (step == 3)
+	if (step == 3 && !sw)
 	{
 		flag[start] = false;
+		sw++;
 	}
-	if (step != 1 && in == start)
+	if (step > 3 && in == start)
 	{
 		count_cycles++;
 	}
@@ -33,37 +36,69 @@ void DSF(int** arr, int size, bool* flag, int in, int start, int& count_cycles)
 			DSF(arr, size, flag, i, start, count_cycles);
 		}
 	}
+	step--;
 }
+
+//int SearchCycles(Graph& graph)
+//{
+//	int n = graph.getsize();
+//	bool* flag = new bool[n];
+//	int count_cycles = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		for (int j = 0; j < n; j++)
+//		{
+//			flag[j] = false;
+//		}
+//		step = 0;
+//		if (!flag[i])
+//		{
+//			DSF(graph.getmatrix(), n, flag, i, i, count_cycles);
+//		}
+//	}
+//	return count_cycles;
+//}
 
 int SearchCycles(Graph& graph)
 {
 	int n = graph.getsize();
 	bool* flag = new bool[n];
-	for (int i = 0; i < n; i++)
-	{
-		flag[i] = false;
-	}
 	int count_cycles = 0;
 	for (int i = 0; i < n; i++)
 	{
-		step = 0;
-		if (!flag[i])
+		int** new_graph = graph.getmatrix();
+		for (int j = 0; j < n; j++)
 		{
-			DSF(graph.getmatrix(), n, flag, i, i, count_cycles);
+			for (int k = 0; k < n; k++)
+			{
+				flag[k] = false;
+			}
+			if (new_graph[i][j])
+			{
+				new_graph[i][j] = 0;
+				new_graph[j][i] = 0;
+				step = 0;
+				sw = 0;
+				DSF(new_graph, n, flag, i, i, count_cycles);
+				new_graph[i][j] = 1;
+				new_graph[j][i] = 1;
+				cout << endl;
+			}
 		}
 	}
 	return count_cycles;
 }
 
-int main() 
+int main()
 {
 	int n, m;
-	cin >> n >> m;
+	fstream file("input.txt");
+	file >> n >> m;
 	Graph g(n);
 	int first, second;
 	for (int i = 0; i < m; i++)
 	{
-		cin >> first >> second;
+		file >> first >> second;
 		g.add_edge(first - 1, second - 1);
 	}
 
